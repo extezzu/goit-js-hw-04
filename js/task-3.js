4698fddcf7fe0bc038d6f6df73c44f31837c49f5
Dmitry Vladimirovich <46675753+extezzu@users.noreply.github.com>
2025-10-05 20:40:30 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,19 @@
+console.group("Task 3");
+
+function filterArray(numbers, value) {
+    let total = [];
+
+    for (const arg of numbers) {
+        if (arg > value) {
+        total.push(arg);
+        }
+    }
+    return total;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+console.groupEnd();
